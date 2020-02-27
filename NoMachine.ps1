9b961bed11aa2d39ab953eace339eb5ddf29b68c
Martin Tobias Skoog <61536588+chill1ne@users.noreply.github.com>
2020-02-27 09:41:58 +0100

    NoMachine.ps1
    NoMachine.ps1

---
+++ NoMachine.ps1
@@ -0,0 +1,13 @@
+{
+    "homepage": "https://www.nomachine.com/",
+    "description": "Freeware Remote Desktop Software",
+    "license": "Freeware",
+    "version": "6.9.2.0",
+    "url": "https://download.nomachine.com/download/6.9/Windows/nomachine_6.9.2_1.exe",
+    "hash": "890254b91ecd6a99da5386f3720b933162840f82cbf8a530f6c3fe239a8f6297",
+    "bin": "gunk.exe",
+    "checkver": "",
+    "autoupdate": {
+        "url": "https://download.nomachine.com/download/$version/Windows/nomachine_$version.exe"
+    }
+}
